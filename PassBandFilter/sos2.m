486992215796148970d881ceae2b97448c5b3ce5
cyprienmaes <cyprienmaes@gmail.com>
2018-05-08 21:25:40 +0200

    Rectification du code octave pour 512 ou 1024

--- PassBandFilter/sos2.m
+++ PassBandFilter/sos2.m
@@ -1,3 +1,3 @@
 function [interval, newval] = sos2(coef, in, w) 
-    interval = in*1024 - (coef(1,5)*w(1,2)) - (coef(1,6)*w(1,1));
-    newval = (coef(1,1)*interval/1024) + (coef(1,2)*w(1,2)) + (coef(1,4)*w(1,1));
+    interval = in*512 - (coef(1,5)*w(1,2)) - (coef(1,6)*w(1,1));
+    newval = (coef(1,1)*interval/512) + (coef(1,2)*w(1,2)) + (coef(1,4)*w(1,1));
