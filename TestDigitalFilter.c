015431594bf9551c717005d38fea58206c4f86cd
cyprienmaes <cyprienmaes@gmail.com>
2018-05-08 21:24:58 +0200

    Transfomation de la simulation en c pour obliger de calculer sur 32bits

--- TestDigitalFilter.c
+++ TestDigitalFilter.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
+#include <stdint.h>
 #include <time.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <math.h>
 
 #define M_PI 3.14159265358979323846
-#define n 1024
-#define nsquare 1048576
-#define gain 8
+
+int32_t n = 512;
+int32_t nsquare =  262144;
 
 typedef struct secondValues {
-    long interval;
-    long newval;
+    int32_t interval;
+    int32_t newval;
 }secondValues;
 
-secondValues sos(long coef[6], long bridge[2], long input) {
+secondValues sos(int32_t coef[6], int32_t bridge[2], int32_t input) {
     secondValues this;
     this.interval = input*n - (coef[4]*bridge[1]) - (coef[5]*bridge[0]);
     this.newval = (coef[0]*this.interval)/n + (coef[1]*bridge[1]) + (coef[2]*bridge[0]);
@@ -25,33 +27,43 @@ secondValues sos(long coef[6], long bridge[2], long input) {
 int main(void)
 {
 
-    /*Coefficients des filtres du second ordre pour 1100Hz*/
-    /*long coef1 [6] = {1024, 2048, 1024, 1024, -1843, 1009};
-    long coef2 [6] = {1024, 2048, 1024, 1024, -1848, 1009};
-    long coef3 [6] = {1024, -2048, 1024, 1024, -1847, 1018};
-    long coef4 [6] = {1024, -2048, 1024, 1024, -1859, 1018};*/
-
-    /*Coefficients des filtres du second ordre pour 900Hz*/
-    long coef1 [6] = {1024, 2048, 1024, 1024, -1907, 1012};
-    long coef2 [6] = {1024, 2048, 1024, 1024, -1911, 1012};
-    long coef3 [6] = {1024, -2048, 1024, 1024, -1911, 1019};
-    long coef4 [6] = {1024, -2048, 1024, 1024, -1920, 1019};
-
+    int32_t coef1 [6] = {1024, 2048, 1024, 1024, -1844, 1009};
+    int32_t coef2 [6] = {1024, 2048, 1024, 1024, -1849, 1009};
+    int32_t coef3 [6] = {1024, -2048, 1024, 1024, -1848, 1018};
+    int32_t coef4 [6] = {1024, -2048, 1024, 1024, -1860, 1018};
+    int gain1100 = 8;
+    /*
+    int32_t coef1 [6] = {1024, 2048, 1024, 1024, -1908, 1012};
+    int32_t coef2 [6] = {1024, 2048, 1024, 1024, -1912, 1012};
+    int32_t coef3 [6] = {1024, -2048, 1024, 1024, -1912, 1019};
+    int32_t coef4 [6] = {1024, -2048, 1024, 1024, -1921, 1019};
+    int gain900 = 6;*/
+    /*
+    int32_t coef1 [6] = {512, 1024, 512, 512, -922, 505};
+    int32_t coef2 [6] = {512, 1024, 512, 512, -925, 547};
+    int32_t coef3 [6] = {512, -1024, 512, 512, -924, 509};
+    int32_t coef4 [6] = {512, -1024, 512, 512, -930, 509};
+    int gain1100 = 4;
+    int32_t coef1 [6] = {512, 1024, 512, 512, -956, 506};
+    int32_t coef2 [6] = {512, 1024, 512, 512, -956, 506};
+    int32_t coef3 [6] = {512, -1024, 512, 512, -956, 509};
+    int32_t coef4 [6] = {512, -1024, 512, 512, -960, 509};
+    int gain900 = 3;*/
     /*Implementation des vecteurs necessaires du filtre (voltage => mots binaires en tension,
                                                          bridge => mots binaires des transitions entre les sos,
                                                          sortie => mots binaires des sorties et de la recursivite)*/
-    long voltage = 0;
-    long bridge1[2] = {0, 0};
-    long output1 = 0;
-    long bridge2[2] = {0, 0};
-    long output2= 0;
-    long bridge3[2] = {0, 0};
-    long output3 = 0;
-    long bridge4[2] = {0, 0};
-    long output4 = 0;
-    long exit = 0;
+    int32_t voltage = 0;
+    int32_t bridge1[2] = {0, 0};
+    int32_t output1 = 0;
+    int32_t bridge2[2] = {0, 0};
+    int32_t output2= 0;
+    int32_t bridge3[2] = {0, 0};
+    int32_t output3 = 0;
+    int32_t bridge4[2] = {0, 0};
+    int32_t output4 = 0;
+    int32_t exit = 0;
     int j = 0;
-    int f = 930;
+    int f = 1200;
     int fe = 16000;
 
     FILE* fichier = NULL;
@@ -64,21 +76,21 @@ int main(void)
             secondValues secondValues1 = sos(coef1, bridge1, voltage);
             bridge1[0] = bridge1[1];
             bridge1[1] = secondValues1.interval/n;
-            output1 = (secondValues1.newval*gain)/nsquare;
+            output1 = (secondValues1.newval*gain1100)/nsquare;
             secondValues secondValues2 = sos(coef2, bridge2, output1);
             bridge2[0] = bridge2[1];
             bridge2[1] = secondValues2.interval/n;
-            output2 = (secondValues2.newval*gain)/nsquare;
+            output2 = (secondValues2.newval*gain1100)/nsquare;
             secondValues secondValues3 = sos(coef3, bridge3, output2);
             bridge3[0] = bridge3[1];
             bridge3[1] = secondValues3.interval/n;
-            output3 = (secondValues3.newval*gain)/nsquare;
+            output3 = (secondValues3.newval*gain1100)/nsquare;
             secondValues secondValues4 = sos(coef4, bridge4, output3);
             bridge4[0] = bridge4[1];
             bridge4[1] = secondValues4.interval/n;
-            output4 = (secondValues4.newval*gain)/nsquare;
+            output4 = (secondValues4.newval*gain1100)/nsquare;
             exit = 512 + output4;
-            fprintf(fichier,  "%ld\n", exit);
+            fprintf(fichier,  "%" PRIi32 "\n", exit);
         }
         fclose(fichier);
     }
